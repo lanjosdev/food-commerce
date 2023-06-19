79a52608de2979798306e4de5faf535f339f7017
Lucas dos Anjos <lucas.anjospro@gmail.com>
2023-06-19 17:17:23 -0300

    Snacks adicionados em todas subpaginas

--- src/pages/Main/Bebidas/index.tsx
+++ src/pages/Main/Bebidas/index.tsx
@@ -1,12 +1,45 @@
 import { Head } from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle";
+import { SnacksContainer } from "../../../components/Snacks";
+
 
 export default function Bebidas() {
+    // lista de bebidas:
+    const data = [
+        {
+          id: 1,
+          snack: 'drink',
+          name: 'Coca-Cola 2L',
+          description: 'A tradicional Coca-Cola que a família brasileira adora.',
+          price: 12,
+          image: 'https://i.imgur.com/Lg3aKhf.jpg',
+        },
+        {
+          id: 2,
+          snack: 'drink',
+          name: 'Guaraná Antarctica',
+          description: 'O irresistível e saboroso Guaraná Antarctica em sua versão de latinha.',
+          price: 6.5,
+          image: 'https://i.imgur.com/hOBrOIm.jpg',
+        },
+        {
+          id: 3,
+          snack: 'drink',
+          name: 'Suco de Abacaxi',
+          description:
+            'Suco natural de abacaxi com leves incrementos de algumas hortaliças para fortificar sua saúde.',
+          price: 8,
+          image: 'https://i.imgur.com/VV9qTMh.jpg',
+        },
+    ];
+
+
     return (
         <>
             <Head title='Bebidas' description="Nossos melhores bebidas" />
 
             <SnackTitle titleSnack='Bebidas' />
+            <SnacksContainer snacks={data} /> 
         </>
     ); 
 }
--- src/pages/Main/Pizzas/index.tsx
+++ src/pages/Main/Pizzas/index.tsx
@@ -1,12 +1,37 @@
 import { Head } from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle";
+import { SnacksContainer } from "../../../components/Snacks";
+
 
 export default function Pizzas() {
+    // lista de pizzas:
+    const data = [
+        {
+            id: 1,
+            snack: 'pizza',
+            name: 'Calabresa',
+            description:
+              'Pizza recheada com calabresa, cebola, mussarela, orégano e azeitona, tendo uma borda recheada com catupiry.',
+            price: 25,
+            image: 'https://i.imgur.com/5rjJGkV.jpg',
+          },
+          {
+            id: 2,
+            snack: 'pizza',
+            name: 'Portuguesa',
+            description:
+              'Pizza recheada com presunto, mussarela, ovo, cebola, azeitona, orégano, tomate e molho de tomate, tendo uma borda recheada com catupiry.',
+            price: 28.5,
+            image: 'https://i.imgur.com/WCoyGoI.png',
+          },
+    ];
+
     return (
         <>
             <Head title='Pizzas' description="Nossos melhores pizzass" />
 
             <SnackTitle titleSnack='Pizzas' />
+            <SnacksContainer snacks={data} />
         </>
     ); 
 }
